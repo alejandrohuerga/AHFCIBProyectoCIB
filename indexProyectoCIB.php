6f9338ef94478f490a973a3a05876fa685d37299
alejandro.dev <alejandro.dev@gmail.com>
2026-01-15 10:16:42 +0100

    feat: :rocket: Nueva estructura index, nuevos estilos y add top 10 OWASP

--- indexProyectoCIB.php
+++ indexProyectoCIB.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../webroot/styles.css">
-    <!--<link rel="stylesheet" href="webroot/css/estilos.css">-->
+    <!--<link rel="stylesheet" href="../webroot/styles.css">-->
+    <link rel="stylesheet" href="webroot/css/estilos.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" type="image/png" href="/Proyecto webs alumnos/media/logoSauces.png">
@@ -19,15 +19,23 @@
     </header>
     <main>
         <section id="asignaturas">
-            <h2>FUNDAMENTOS DE CIBERSEGURIDAD</h2>
-            <ul>
-                <li><a href="webroot/images/DocumentacionTema1.pdf" target="_blank">GLOSARIO</a></li>
-                <li><a href="https://github.com/alejandrohuerga/AHFDAWProyectoDAw" target="_blank">DOCUMENTACIÓN SERVIDOR WEB SEGURO</a></li>
-                <li><a href="webroot/images/251103ProtocoloInstalaciónAlejandroDeLaHuerga.pdf" target="_blank">PROTOCOLO DE INSTALACIÓN</a></li>
-                <li><a href="webroot/images/guia_glosario_ciberseguridad_2021.pdf" target="_blank">GLOSARIO TÉRMINOS INCIBE</a></li>
-            </ul>
+                <div class="container" id="tema3">
+                    <h3>UT1:CIBERSEGURIDAD</h3>
+                    <li><a href="webroot/images/DocumentacionTema1.pdf" target="_blank">GLOSARIO</a></li>
+                    <li><a href="webroot/images/guia_glosario_ciberseguridad_2021.pdf" target="_blank">GLOSARIO TÉRMINOS INCIBE</a></li>
+                    <li><a href="webroot/images/OWASP TOP 10 VULNEARIBILIDADES 1501.pdf" target="_blank">OWASP TOP 10 VULNERABILIDADES</a></li>
+                </div>
+                <div class="container" id="tema2">
+                    <h3>UT2:SERVIDOR WEB SEGURO</h3>
+                    <li><a href="https://github.com/alejandrohuerga/AHFDAWProyectoDAw" target="_blank">DOCUMENTACIÓN SERVIDOR WEB SEGURO</a></li>
+                    <li><a href="webroot/images/251103ProtocoloInstalaciónAlejandroDeLaHuerga.pdf" target="_blank">PROTOCOLO DE INSTALACIÓN</a></li>
+                </div>
+                <div class="container" id="tema3">
+                    <h3>UT3:APLICACIÓN WEB SEGURA</h3>
+                    <li><a href="../AHFDWESLoginLogoffTema5/indexLoginLogoffTema5.php">LoginLoggoffTema5</a></li>
+                    <li><a href="../AHFDWESLoginLogoff/indexLoginLogoff.php">LoginLoggoff</a></li>
+                </div>
         </section>
-        
     </main>
     <footer>
         <p><a href="../index.html">&copy; 2024 Alejandro De la Huerga. Todos los derechos reservados.</a></p>
